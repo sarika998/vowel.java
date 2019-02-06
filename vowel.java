a89a687e9fd22a7c6b44cd6ad8e4ba0860c61754
sarika998 <47376634+sarika998@users.noreply.github.com>
2019-02-06 16:15:18 +0530

    Create vowel.java

---
+++ vowel.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+      char ch = 'i';
+      if(ch == 'a'|| ch == 'e'|| ch == 'i' ||ch == 'o' ||ch == 'u')
+      {
+         System.out.println("Vowel");
+      }
+      else
+      {
+         System.out.println("Invalid");
+      }
+   }
+}
